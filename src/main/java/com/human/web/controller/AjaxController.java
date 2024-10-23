6c6164262aad07b93f87fb2465d93c2cafcd5d83
ParkYeseulzz <yesul_park@naver.com>
2024-10-23 16:59:18 +0900

    ajax테스트

--- src/main/java/com/human/web/controller/AjaxController.java
+++ src/main/java/com/human/web/controller/AjaxController.java
@@ -1,6 +1,6 @@
 package com.human.web.controller;
 
-
+//수정테스트
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,44 +12,44 @@ import lombok.RequiredArgsConstructor;
 @RestController //@Controller + @ResponseBody
 @RequiredArgsConstructor
 public class AjaxController {
-	
+
 	//필드 정의
 	private final M_MemberService m_memberServiceImpl;
 	
-	
+
 	// 아이디 중복검사
-    @PostMapping("/Member/checkId")
-    public String checkId(@RequestParam("m_email") String m_email) {
+	@PostMapping("/Member/checkId")
+	public String checkId(@RequestParam("m_email") String m_email) {
         String result = "PASS";  // 중복된 아이디가 없는 경우 기본값
 
-        try {
-            if (m_memberServiceImpl.checkId(m_email) >= 1) {
-                result = "FAIL";
-            }
-        } catch (Exception e) {
-            System.out.println("아이디 중복 검사 중 오류 발생: " + e.getMessage());
-            e.printStackTrace();
-        }
+		try {
+			if (m_memberServiceImpl.checkId(m_email) >= 1) {
+				result = "FAIL";
+			}
+		} catch (Exception e) {
+			System.out.println("아이디 중복 검사 중 오류 발생: " + e.getMessage());
+			e.printStackTrace();
+		}
 
-        return result;
-    }
+		return result;
+	}
     
-    
-    // 닉네임 중복 체크
-    @PostMapping("/Member/checkNickname")
-    public String checkNickname(@RequestParam("m_nickname") String m_nickname) {
-    	String result = "PASS";
-    	
-    	try {
+
+	// 닉네임 중복 체크
+	@PostMapping("/Member/checkNickname")
+	public String checkNickname(@RequestParam("m_nickname") String m_nickname) {
+		String result = "PASS";
+
+		try {
 			if(m_memberServiceImpl.checkNickname(m_nickname) >= 1) {
 				result = "FAIL";
 			}
 		} catch (Exception e) {
-			 System.out.println("닉네임 중복 검사 중 오류 발생: " + e.getMessage());
-	            e.printStackTrace();
+			System.out.println("닉네임 중복 검사 중 오류 발생: " + e.getMessage());
+			e.printStackTrace();
 		}
-    	return result;
-    }
-    
+		return result;
+	}
+
 		
 }
